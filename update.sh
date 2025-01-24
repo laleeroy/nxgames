d97c813c4a118f2bdfc8f888c8b103d9bb009486
laleeroy <gabwielcastwo@gmail.com>
2025-01-24 12:06:57 +0800

    Add header for when the last update happen
    Signed-off-by: laleeroy <gabwielcastwo@gmail.com>

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Get the current date and time
-date=$(date +"%Y-%m-%d")
-time=$(date +"%H:%M:%S")
+date=$(date +"%b %d %Y")
+time=$(date +"%I:%M%p")
 
 # Change working dir
 cd /home/pi5/nxgames
@@ -10,6 +10,9 @@ cd /home/pi5/nxgames
 # Run multiple scripts
 python3 /home/pi5/.bin/nxgames-rename /srv/dev-disk-by-uuid-b37d7069-0eb1-4ce4-bb3b-2f896c6f8b45/Games/Switch
 bash /home/pi5/.bin/nxgames-update /srv/dev-disk-by-uuid-b37d7069-0eb1-4ce4-bb3b-2f896c6f8b45/Games/Switch
+
+# Update index.html with the current date and time
+sed -i "s|<p id=\"credit-text\">.*</p>|<p id=\"credit-text\">Updated as of $date $time</p>|" index.html
 
 # Git things done
 git add .
